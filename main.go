b79ec8d3eae556c58283ec482b62cd92dacd8154
apxxxxxxe <calcium629@gmail.com>
2022-05-22 15:08:57 +0900

    update: さくらスクリプト削除処理を変更 - saori-basic側で処理するようにした - クイックセクションを削除するように

--- main.go
+++ main.go
@@ -7,7 +7,100 @@ import (
 	"log"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
+
+func clearTags(src string) string {
+	return regexp.MustCompile(`\\_{0,2}[a-zA-Z0-9*!&](\d|\[("([^"]|\\")+?"|([^\]]|\\\])+?)+?\])?`).ReplaceAllString(src, "")
+}
+
+func deleteQuickSection(src string) string {
+	var result string
+
+	min := func(x, y int) int {
+		if x > y {
+			return y
+		} else {
+			return x
+		}
+	}
+
+	getStartPoint := func(s string) (int, int) {
+		var (
+			result int
+			length int
+		)
+		quickStart := "\\![quicksection,true]"
+		quick := "\\_q"
+		index_true := strings.Index(s, quickStart)
+		index := strings.Index(s, quick)
+
+		if index_true != -1 && index != -1 {
+			if index < index_true {
+				result = index
+				length = len(quick)
+			} else {
+				result = index_true
+				length = len(quickStart)
+			}
+		} else if index_true == -1 && index != -1 {
+			result = index
+			length = len(quick)
+		} else if index_true != -1 && index == -1 {
+			result = index_true
+			length = len(quickStart)
+		} else {
+			result = -1
+			length = 0
+		}
+
+		return result, length
+	}
+
+	getEndPoint := func(s string) int {
+		var result int
+		quickEnd := "\\![quicksection,false]"
+		quick := "\\_q"
+		index_end := strings.Index(s, quickEnd)
+		index := strings.Index(s, quick)
+
+		if index_end != -1 && index != -1 {
+			result = min(index_end+len(quickEnd), index+len(quick))
+		} else if index_end == -1 && index != -1 {
+			result = index + len(quick)
+		} else if index_end != -1 && index == -1 {
+			result = index_end + len(quickEnd)
+		} else {
+			result = -1
+		}
+
+		return result
+	}
+
+	isquick := false
+	for {
+		if !isquick {
+			i, l := getStartPoint(src)
+			if i == -1 {
+				result += src
+				break
+			}
+			result += src[:i]
+			src = src[i+l:]
+			isquick = true
+		} else {
+			i := getEndPoint(src)
+			if i == -1 {
+				break
+			}
+			src = src[i:]
+			isquick = false
+		}
+	}
+
+	return result
+}
 
 func main() {
 
@@ -15,10 +108,12 @@ func main() {
 		panic(errors.New("invalid arguments"))
 	}
 
-	msg, err := base64.StdEncoding.DecodeString(os.Args[1])
+	rawMsg, err := base64.StdEncoding.DecodeString(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
+	msg := []byte(clearTags(deleteQuickSection(string(rawMsg))))
+
 	msg_length := uint32(len(msg))
 	iCommand := []byte{1, 0}
 	iSpeed := []byte{255, 255}
